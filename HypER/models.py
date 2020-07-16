fae7d7edbe0d03bcef0c4d34ce75158b5d17da89
xhosaBoy <luyolo.nqobile@gmail.com>
2020-07-16 17:16:46 +0200

    HypER Relation Normalisation
    Change nonlinearity to Tanh from ReLU.

--- HypER/models.py
+++ HypER/models.py
@@ -131,7 +131,7 @@ class HypER(torch.nn.Module):
         k = self.relational_filter_drop(k)
 
         x = F.conv2d(x, k, groups=e1.size(0))
-        x = F.relu(x)
+        x = F.tanh(x)
 
         # depthwise convolution
         x = x.view(e1.size(0), 1, self.out_channels, 1 - self.filt_h + 1, e1.size(3) - self.filt_w + 1)
@@ -146,7 +146,7 @@ class HypER(torch.nn.Module):
         x = x.view(e1.size(0), -1)
 
         x = self.fc(x)
-        x = F.relu(x)
+        x = F.tanh(x)
 
         # Hidden layer regularisation
         x = self.bn4(x)
