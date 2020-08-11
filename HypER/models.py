e0e27682845f6aa5ed7d6d9e119ad8a687fbfcc4
xhosaBoy <luyolo.nqobile@gmail.com>
2020-08-11 05:30:08 +0200

    HypER Relation Normalisation
    Add hidden layer dropout.

--- HypER/models.py
+++ HypER/models.py
@@ -124,6 +124,9 @@ class HypER(torch.nn.Module):
         x = self.fc(x)
         x = F.relu(x)
 
+        # Hidden layer regularisation
+        x = self.hidden_drop(x)
+
         x = torch.mm(x, self.E.weight.transpose(1,0))
         x += self.b.expand_as(x)
 
